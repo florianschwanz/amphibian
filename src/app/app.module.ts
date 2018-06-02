4e494ec707c663f878c9e4732c172f4db53968c9
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-06-02 22:12:11 +0200

    Updated to Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
-import {MaterialModule} from "@angular/material";
-import {PlatformService} from "./services/platform.service";
-import {ResponsiveModule} from "ng2-responsive";
+import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {PlatformService} from './services/platform.service';
+import {NgModule} from '@angular/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule,
-    ResponsiveModule
+    BrowserModule
   ],
   providers: [PlatformService],
   bootstrap: [AppComponent]
 })
-
-export class AppModule {
-}
+export class AppModule { }
