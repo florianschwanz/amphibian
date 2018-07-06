be878ef1e119279b1724d9fb63225ca228dcc364
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-07-06 19:51:22 +0200

    Fixed some lint issues

--- src/app/services/platform.service.ts
+++ src/app/services/platform.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from "@angular/core";
-import {OperatingSystem} from "../model/operating-system.enum";
+import {Injectable} from '@angular/core';
+import {OperatingSystem} from '../model/operating-system.enum';
 
 @Injectable()
 export class PlatformService {
@@ -7,9 +7,17 @@ export class PlatformService {
   operatingSystem: OperatingSystem;
 
   constructor() {
-    if (navigator.appVersion.indexOf("Win") != -1) this.operatingSystem = OperatingSystem.WINDOWS;
-    if (navigator.appVersion.indexOf("Mac") != -1) this.operatingSystem = OperatingSystem.MAC;
-    if (navigator.appVersion.indexOf("X11") != -1) this.operatingSystem = OperatingSystem.UNIX;
-    if (navigator.appVersion.indexOf("Linux") != -1) this.operatingSystem = OperatingSystem.LINUX;
+    if (navigator.appVersion.indexOf('Win') !== -1) {
+      this.operatingSystem = OperatingSystem.WINDOWS;
+    }
+    if (navigator.appVersion.indexOf('Mac') !== -1) {
+      this.operatingSystem = OperatingSystem.MAC;
+    }
+    if (navigator.appVersion.indexOf('X11') !== -1) {
+      this.operatingSystem = OperatingSystem.UNIX;
+    }
+    if (navigator.appVersion.indexOf('Linux') !== -1) {
+      this.operatingSystem = OperatingSystem.LINUX;
+    }
   }
 }
